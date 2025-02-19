9d5f267ad6e425c973a649cb10c1223c92a29a56
yungiy <devyungiy@gmail.com>
2025-02-19 15:43:59 +0900

    [update]
    - react basi
    - 컴포넌트 만들기
    - 마크업 추가
    - 데이터 표시
    - 조건과 리스트
    - 이벤트 응답&화면 업데이트
    - 컴포넌트간 데이터 공유

--- react-official-document/quick-start/src/App.jsx
+++ react-official-document/quick-start/src/App.jsx
@@ -1,33 +1,132 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 컴포넌트 생성 및 중첩
+//React 컴포넌트의 이름은 항상 대문자로 시작해야 하고 HTML 태그는 소문자로 시작해야 합니다.
+function MyButton() {
+  return (
+    <button onClick={() => {
+    }}>나는 버튼</button>
+  )
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+function Component() {
+  return (
+    <div style={{border: '1px solid #ccc', padding: '1rem'}}>
+      <h1>안녕</h1>
+      <MyButton/>
+    </div>
+  )
+}
+
+// 데이터 표시하기
+const user = {
+  name: 'Kendrick Lamar',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90
+};
+
+function Profile() {
+  return (
+    <div style={{border: '1px solid #ccc', padding: '1rem'}}>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </div>
+  )
+}
+
+// 리스트렌더링
+const products = [
+  {title: 'medicine', isharmful: false, id: 1},
+  {title: 'drug', isharmful: true, id: 2},
+  {title: 'hemp', isharmful: true, id: 3}
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isharmful ? 'black' : 'red'
+      }}
+    >
+      {product.title}
+    </li>
+  );
 
   return (
+    <div style={{border: '1px solid #ccc', padding: '1rem'}}>
+      <ul>{listItems}</ul>
+    </div>
+  );
+}
+
+// 화면 렌더링
+import {useState} from 'react';
+
+function MyApp() {
+  return (
+    <div style={{border: '1px solid #ccc', padding: '1rem'}}>
+      <h1>각각 올라가는 버튼</h1>
+      <MyButton2/>
+      <MyButton2/>
+    </div>
+  );
+}
+
+function MyButton2() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+// 훅을 이용한 설계 모두 같이 올라가는 버튼
+function MyApp2() {
+  const [countBtn, setCountBtn] = useState(0);
+
+  function handleClick() {
+    setCountBtn(countBtn + 1);
+  }
+
+  return (
+    <div style={{border: '1px solid #ccc', padding: '1rem'}}>
+      <h1>모두 같이 올라가는 버튼</h1>
+      <MyButton3 count={countBtn} onClick={handleClick}/>
+      <MyButton3 count={countBtn} onClick={handleClick}/>
+    </div>
+  );
+}
+
+//eslint-disable-next-line react/prop-types
+function MyButton3({onClick, count}) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+function App() {
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Component/>
+      <Profile/>
+      <ShoppingList/>
+      <MyApp/>
+      <MyApp2/>
     </>
   )
 }
